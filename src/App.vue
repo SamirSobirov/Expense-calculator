5288decc77d87777a5e0106fbb49cbd4f70ef914
Sobirov S <sobiroff23@gmail.com>
2024-12-19 11:29:06 +0500

    fix: appVue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
-import { AddTransactionForm } from "./components/AddTransactionForm.vue";
-import { TransactionList } from "./components/TransactionList.vue";
-import { BalanceSummary } from "./components/BalanceSummary.vue";
+import AddTransactionForm from "./components/AddTransactionForm.vue";
+import TransactionList from "./components/TransactionList.vue";
+import BalanceSummary from "./components/BalanceSummary.vue";
 
 import { useFinanceStore } from "./stores/useFinanceStore";
-
 const financeStore = useFinanceStore();
 
 onMounted(() => {
